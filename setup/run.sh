09b90558067fbed8fdf06087a3868ef383f6c8d7
gfdgd_xi <3025613752@qq.com>
2024-06-10 16:47:43 +0800

    新增loader以解决缺少PATH环境变量导致的问题

--- setup/run.sh
+++ setup/run.sh
@@ -4,8 +4,16 @@ export LD_LIBRARY_PATH=$(pwd)
 export PROOT_TMP_DIR=$(pwd)/tmp
 export PROOT_LOADER=$(pwd)/loader
 
+export BOTTLEPATH=debian12_aarch64
+# 拷贝 loader
+if [[ ! -f $$BOTTLEPATH ]]; then
+  cp -v loader.sh $BOTTLEPATH/loader.sh
+fi
+
+cp loader.sh
+
 ./proot \
-  --bind=debian12_aarch64/tmp:/dev/shm \
+  --bind=$BOTTLEPATH/tmp:/dev/shm \
   --bind=/sys \
   --bind=/proc/self/fd/2:/dev/stderr \
   --bind=/proc/self/fd/1:/dev/stdout \
@@ -21,6 +29,6 @@ export PROOT_LOADER=$(pwd)/loader
   --kill-on-exit \
   --cwd=/ \
   --change-id=0:0 \
-  --rootfs=debian12_aarch64 \
-  /bin/bash "$@"
+  --rootfs=$BOTTLEPATH \
+  /bin/bash /loader.sh "$@"
 
