607f116578f0d1528b46385bf5688c4400097e82
gfdgd_xi <3025613752@qq.com>
2024-07-01 14:51:13 +0800

    移除无用文件

--- proot-distro/run.sh
+++ proot-distro/run.sh
@@ -3,7 +3,7 @@ export PATH=$(pwd)/files/usr/bin:$PATH
 export LD_LIBRARY_PATH=$(pwd)/files/usr/lib
 export PROOT_TMP_DIR=$(pwd)/cache
 export PROOT_LOADER=$(pwd)/files/usr/libexec/proot/loader
-export PROOT_LOADER_32=$(pwd)/files/usr/libexec/proot/loader32
+export PROOT_LOADER_32=$(pwd)/files/usr/libexec/proot/loader32  # 用于运行 armhf 应用
 export LD_PRELOAD=$(pwd)/files/usr/lib/libtermux-exec.so
 bash $(pwd)/files/usr/bin/neofetch
 bash $(pwd)/files/usr/bin/proot-distro login debian
