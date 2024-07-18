329b05e228767c69684eabd7a64f69a5ae34769c
gfdgd_xi <3025613752@qq.com>
2024-07-18 11:44:26 +0800

    移除废弃文件、支持开启pkexec授权服务、修改容器名称等

--- proot-distro/loader.sh
+++ proot-distro/loader.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+# 参数 1 为分辨率宽，2为高
 export DISPLAY=:1
 # 修改默认语言
 export LANG=zh_CN.UTF-8
@@ -8,28 +9,25 @@ export QTWEBENGINE_DISABLE_SANDBOX=1
 # 删除文件以便 vncserver 可以正常在 :1 开启
 rm -rfv /tmp/.X1*
 cd /tmp
-# 开启 NoVNC 服务（只允许本机访问）
-/novnc/utils/novnc_proxy --vnc localhost:5901 --listen localhost:6080 &
 # 开启 VNC 服务（只允许本机访问）
-vncserver :$DISPLAY -localhost
+if [ $# -eq 0 ]; then
+	vncserver $DISPLAY #-localhost
+else
+	vncserver $DISPLAY -geometry ${1}x${2} #-localhost
+fi
 # 打开窗口管理器
-icewm &
-deepin-wine-runner &
+startxfce4 &
+xfce4-terminal &
+# 开启 NoVNC 服务（只允许本机访问）
+/novnc/utils/novnc_proxy --vnc localhost:5901 & #--listen localhost:6080 &
 echo 等待用户自行退出
-while true do
+while true ; do
 	monitor=`ps -ef | grep novnc_proxy | grep -v grep | wc -l ` 
 	# 检查 vnc 服务是否正常运行
 	if [ $monitor -eq 0 ] 
 	then
 		echo "NoVNC had exited, restart"
 		/novnc/utils/novnc_proxy --vnc localhost:5901 --listen localhost:6080 &
-	fi
-	monitor=`ps -ef | grep icewm | grep -v grep | wc -l ` 
-	# 检查 icewm 服务是否正常运行
-	if [ $monitor -eq 0 ] 
-	then
-		echo "NoVNC had exited, restart"
-		icewm > /dev/null &
 	fi
 	sleep 10
 done
--- proot-distro/run.sh
+++ proot-distro/run.sh
@@ -1,15 +1,21 @@
 #!/system/bin/sh
 CURRENT_DIR=$(cd $(dirname $0); pwd)
+mkdir -p $CURRENT_DIR/cache
 export PATH=$CURRENT_DIR/files/usr/bin:$PATH
 export LD_LIBRARY_PATH=$CURRENT_DIR/files/usr/lib
-export PROOT_TMP_DIR=$CURRENT_DIR/cache
+export PROOT_TMP_DIR=/data/data/top.gfdgdxi.wine.runner.android/cache
 export PROOT_LOADER=$CURRENT_DIR/files/usr/libexec/proot/loader
 export PROOT_LOADER_32=$CURRENT_DIR/files/usr/libexec/proot/loader32  # 用于运行 armhf 应用
 export LD_PRELOAD=$CURRENT_DIR/files/usr/lib/libtermux-exec.so
+export HOME=$CURRENT_DIR/files/home/
+export TMPDIR=$CURRENT_DIR/files/usr/tmp/
 bash $CURRENT_DIR/files/usr/bin/neofetch
+pulseaudio --start --exit-idle-time=-1
+pacmd load-module module-native-protocol-tcp auth-ip-acl=127.0.0.1 auth-anonymous=1
+
 if [ $# -eq 0 ]; then
-    bash $CURRENT_DIR/files/usr/bin/proot-distro login debian
+    bash $CURRENT_DIR/files/usr/bin/proot-distro login winerunner
 else
-    bash $CURRENT_DIR/files/usr/bin/proot-distro login debian -- $*
+    bash $CURRENT_DIR/files/usr/bin/proot-distro login winerunner -- $*
 fi
 exit $?
